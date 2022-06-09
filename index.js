97e37d3fe07fa2782b7fdadabf5b716390545027
Trevor Lister <Trevor Lister>
2022-06-09 19:01:24 -0300

    Adds infinite fish loops

--- index.js
+++ index.js
@@ -1,2 +1,61 @@
-const content = document.getElementById('content')
-content.textContent = 'The Fish Tank'
+
+  const content = document.getElementById('content')
+  content.textContent = 'The Fish Tank'
+
+  const fishTankGlass = document.getElementById('fish-tank-glass');
+
+ 
+  const fishPosition = Math.floor(Math.random() * 3)
+
+  // fishTankGlass.appendChild()
+  console.log(fishTankGlass)
+
+  const arr = ['red-fish', 'blue-fish', 'purple-fish']
+  
+  const makeFishes = (fish) => {
+    const testFish = document.createElement('img')
+    testFish.id = `test`
+  testFish.classList.add(arr[fish])
+  testFish.classList.add('fish')  
+  fishTankGlass.appendChild(testFish)
+    
+  
+  }
+
+  
+ console.log(fishPosition)
+  
+
+  const removeFish = async () => {
+    fishTankGlass.removeChild(document.getElementById('test'))
+  }
+
+  //1. Create a new function that returns a promise
+  function firstFunction() {
+    return new Promise((resolve, reject) => {
+        let y = 0
+        setTimeout(() => {
+          const result = Math.floor(Math.random() * 3)
+           makeFishes(result)
+           console.log('Loop completed.')  
+           resolve(y)
+        }, 2000)
+    })
+  }
+  
+  //2. Create an async function
+  async function secondFunction() {
+      console.log('Before promise call.')
+      //3. Await for the first function to complete
+      const result = await firstFunction()
+      setTimeout(removeFish, 3000)
+      secondFunction()
+      console.log('Promise resolved: ' + result)
+      console.log('Next step.')
+  }; 
+
+  
+  
+  
+    secondFunction()
+  
